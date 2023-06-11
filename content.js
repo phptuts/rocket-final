0038b77b76cccaf9b5fed48b38dd3907f52e766c
phptuts <glaserpower@gmail.com>
2023-06-10 22:49:44 -0700

    5-blastoff-challenge

--- content.js
+++ content.js
@@ -1,3 +1,9 @@
+/*
+Create a function that allows you send a blast off with 
+an image url and start location.  Use setTimeout to fire the 
+function multiple times at differnt x positions.
+*/
+
 chrome.runtime.onMessage.addListener(async (message, sender, reply) => {
   console.log("message", message);
   reply("ok");
@@ -7,6 +13,16 @@ chrome.runtime.onMessage.addListener(async (message, sender, reply) => {
     alert("url not set for blastoff!");
     return;
   }
+  fireImage(url, 200);
+  setTimeout(() => {
+    fireImage(url, 400);
+  }, 400);
+  setTimeout(() => {
+    fireImage(url, 600);
+  }, 500);
+});
+
+function fireImage(url, xPosition) {
   const img = document.createElement("img");
   img.src = url;
   img.style.width = "100px";
@@ -14,7 +30,7 @@ chrome.runtime.onMessage.addListener(async (message, sender, reply) => {
   img.style.position = "absolute";
   img.style.zIndex = 1000;
   document.body.append(img);
-  let x = 200;
+  let x = xPosition;
   let y = window.screen.height;
   let intervalId = setInterval(() => {
     img.style.left = x + "px";
@@ -26,4 +42,4 @@ chrome.runtime.onMessage.addListener(async (message, sender, reply) => {
       img.remove();
     }
   }, 5);
-});
+}
