eb68c46a8a9cd187c238b48cdbb9d54f9842412b
phptuts <glaserpower@gmail.com>
2023-06-10 22:39:14 -0700

    4-blastoff-image

--- content.js
+++ content.js
@@ -1,4 +1,29 @@
-chrome.runtime.onMessage.addListener((message, sender, reply) => {
+chrome.runtime.onMessage.addListener(async (message, sender, reply) => {
   console.log("message", message);
   reply("ok");
+  const data = await chrome.storage.local.get(["options"]);
+  const url = data?.options?.url;
+  if (!url) {
+    alert("url not set for blastoff!");
+    return;
+  }
+  const img = document.createElement("img");
+  img.src = url;
+  img.style.width = "100px";
+  img.style.height = "100px";
+  img.style.position = "absolute";
+  img.style.zIndex = 1000;
+  document.body.append(img);
+  let x = 200;
+  let y = window.screen.height;
+  let intervalId = setInterval(() => {
+    img.style.left = x + "px";
+    img.style.top = y + "px";
+    y -= 5;
+    x += 2;
+    if (y < -500) {
+      clearInterval(intervalId);
+      img.remove();
+    }
+  }, 5);
 });
